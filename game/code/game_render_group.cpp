c2ad702c9eb57b84ef1c0c3d661413b2d728c06f
Joshalosh <josh.jessopsmith@gmail.com>
2021-12-15 09:32:07 +1100

    Add pushbuffer entity types

--- game/code/game_render_group.cpp
+++ game/code/game_render_group.cpp
@@ -210,6 +210,20 @@ DrawMatte(loaded_bitmap *Buffer, loaded_bitmap *Bitmap,
     }
 }
 
+inline v2 GetRenderEntityBasisP(render_group *RenderGroup, render_entity_basis *EntityBasis, v2 ScreenCentre)
+{
+    v3 EntityBaseP = EntityBasis->Basis->P;
+    real32 ZFudge = (1.0f + 0.1f*(EntityBaseP.Z + EntityBasis->OffsetZ));
+
+    real32 EntityGroundPointX = ScreenCentre.X + RenderGroup->MetresToPixels*ZFudge*EntityBaseP.X;
+    real32 EntityGroundPointY = ScreenCentre.Y - RenderGroup->MetresToPixels*ZFudge*EntityBaseP.Y;
+    real32 EntityZ = -RenderGroup->MetresToPixels*EntityBaseP.Z;
+
+    v2 Centre = {EntityGroundPointX + EntityBasis->Offset.X,
+                 EntityGroundPointY + EntityBasis->Offset.Y + EntityBasis->EntityZC*EntityZ};
+    return(Centre);
+}
+
 internal void
 RenderGroupToOutput(render_group *RenderGroup, loaded_bitmap *OutputTarget)
 {
@@ -217,43 +231,36 @@ RenderGroupToOutput(render_group *RenderGroup, loaded_bitmap *OutputTarget)
 
     for(uint32_t BaseAddress = 0; BaseAddress < RenderGroup->PushBufferSize;)
     {
-        render_group_entry *TypelessEntry = (render_group_entry *)(RenderGroup->PushBufferBase + BaseAddress);
-        switch(TypelessEntry->Type)
+        render_group_entry_header *Header = (render_group_entry_header *)(RenderGroup->PushBufferBase + BaseAddress);
+        switch(Header->Type)
         {
-            case RenderGroupEntry_Clear:
+            case RenderGroupEntryType_render_entry_clear:
             {
-                render_entry_clear *Entry = (render_entry_clear *)Entry;
+                render_entry_clear *Entry = (render_entry_clear *)Header;
 
                 BaseAddress += sizeof(*Entry);
             } break;
 
-            case RenderGroupEntry_Rectangle:
+            case RenderGroupEntryType_render_entry_bitmap:
             {
-                render_entry_rectangle *Entry = (render_entry_rectangle *)Entry;
-                
+                render_entry_bitmap *Entry = (render_entry_bitmap *)Header;
+                v2 P = GetRenderEntityBasisP(RenderGroup, &Entry->EntityBasis, ScreenCentre);
+                Assert(Entry->Bitmap);
+                DrawBitmap(OutputTarget, Entry->Bitmap, P.X, P.Y, Entry->A);
+
                 BaseAddress += sizeof(*Entry);
             } break;
 
-            InvalidDefaultCasse;
-        }
+            case RenderGroupEntryType_render_entry_rectangle:
+            {
+                render_entry_rectangle *Entry = (render_entry_rectangle *)Header;
+                v2 P = GetRenderEntityBasisP(RenderGroup, &Entry->EntityBasis, ScreenCentre);
+                DrawRectangle(OutputTarget, P, P + Entry->Dim, Entry->R, Entry->G, Entry->B);
 
-        v3 EntityBaseP = Entry->Basis->P;
-        real32 ZFudge = (1.0f + 0.1f*(EntityBaseP.Z + Entry->OffsetZ));
+                BaseAddress += sizeof(*Entry);
+            } break;
 
-        real32 EntityGroundPointX = ScreenCentre.X + RenderGroup->MetresToPixels*ZFudge*EntityBaseP.X;
-        real32 EntityGroundPointY = ScreenCentre.Y - RenderGroup->MetresToPixels*ZFudge*EntityBaseP.Y;
-        real32 EntityZ = -RenderGroup->MetresToPixels*EntityBaseP.Z;
-
-        v2 Center = {EntityGroundPointX + Entry->Offset.X,
-                     EntityGroundPointY + Entry->Offset.Y + Entry->EntityZC*EntityZ};
-        if(Entry->Bitmap)
-        {
-            DrawBitmap(OutputTarget, Entry->Bitmap, Center.X, Center.Y, Entry->A);
-        }
-        else
-        {
-            v2 HalfDim = 0.5f*RenderGroup->MetresToPixels*Entry->Dim;
-            DrawRectangle(OutputTarget, Center - HalfDim, Center + HalfDim, Entry->R, Entry->G, Entry->B);
+            InvalidDefaultCase;
         }
     }
 
@@ -273,5 +280,87 @@ AllocateRenderGroup(memory_arena *Arena, uint32_t MaxPushBufferSize, real32 Metr
     Result->PushBufferSize = 0;
 
     return(Result);
+}
+
+#define PushRenderElement(Group, type) (type *)PushRenderElement_(Group, sizeof(type), RenderGroupEntryType_##type)
+inline render_group_entry_header *
+PushRenderElement_(render_group *Group, uint32_t Size, render_group_entry_type Type)
+{
+    render_group_entry_header *Result = 0;
+
+    if((Group->PushBufferSize + Size) < Group->MaxPushBufferSize)
+    {
+        Result = (render_group_entry_header *)(Group->PushBufferBase + Group->PushBufferSize);
+        Result->Type = Type;
+        Group->PushBufferSize += Size;
+    }
+    else
+    {
+        InvalidCodePath;
+    }
+
+    return(Result);
+}
+
+inline void
+PushPiece(render_group *Group, loaded_bitmap *Bitmap,
+          v2 Offset, real32 OffsetZ, v2 Align, v2 Dim, v4 Color, real32 EntityZC)
+{
+    render_entry_bitmap *Piece = PushRenderElement(Group, render_entry_bitmap);
+    if(Piece)
+    {
+        Piece->EntityBasis.Basis = Group->DefaultBasis;
+        Piece->Bitmap = Bitmap;
+        Piece->EntityBasis.Offset = Group->MetresToPixels*V2(Offset.X, -Offset.Y) - Align;
+        Piece->EntityBasis.OffsetZ = OffsetZ;
+        Piece->EntityBasis.EntityZC = EntityZC;
+        Piece->R = Color.R;
+        Piece->G = Color.G;
+        Piece->B = Color.B;
+        Piece->A = Color.A;
+    }
+}
+
+inline void
+PushBitmap(render_group *Group, loaded_bitmap *Bitmap,
+           v2 Offset, real32 OffsetZ, v2 Align, real32 Alpha = 1.0f, real32 EntityZC = 1.0f)
+{
+    PushPiece(Group, Bitmap, Offset, OffsetZ, Align, V2(0, 0), V4(1.0f, 1.0f, 1.0f, Alpha), EntityZC);
+}
+
+inline void
+PushRect(render_group *Group, v2 Offset, real32 OffsetZ,
+         v2 Dim, v4 Color, real32 EntityZC = 1.0f)
+{
+    render_entry_rectangle *Piece = PushRenderElement(Group, render_entry_rectangle);
+    if(Piece)
+    {
+        v2 HalfDim = 0.5f*Group->MetresToPixels*Dim;
+
+        Piece->EntityBasis.Basis = Group->DefaultBasis;
+        Piece->EntityBasis.Offset = Group->MetresToPixels*V2(Offset.X, -Offset.Y) - HalfDim;
+        Piece->EntityBasis.OffsetZ = OffsetZ;
+        Piece->EntityBasis.EntityZC = EntityZC;
+        Piece->R = Color.R;
+        Piece->G = Color.G;
+        Piece->B = Color.B;
+        Piece->A = Color.A;
+        Piece->Dim = Group->MetresToPixels*Dim;
+    }
+}
+
+inline void
+PushRectOutline(render_group *Group, v2 Offset, real32 OffsetZ,
+                v2 Dim, v4 Color, real32 EntityZC = 1.0f)
+{
+    real32 Thickness = 0.1f;
+
+    // Top and Bottom.
+    PushPiece(Group, 0, Offset - V2(0, 0.5f*Dim.Y), OffsetZ, V2(0, 0), V2(Dim.X, Thickness), Color, EntityZC);
+    PushPiece(Group, 0, Offset + V2(0, 0.5f*Dim.Y), OffsetZ, V2(0, 0), V2(Dim.X, Thickness), Color, EntityZC);
+
+    // Left and Right.
+    PushPiece(Group, 0, Offset - V2(0.5f*Dim.X, 0), OffsetZ, V2(0, 0), V2(Thickness, Dim.Y), Color, EntityZC);
+    PushPiece(Group, 0, Offset + V2(0.5f*Dim.X, 0), OffsetZ, V2(0, 0), V2(Thickness, Dim.Y), Color, EntityZC);
 }
 
--- game/code/game_render_group.h
+++ game/code/game_render_group.h
@@ -5,11 +5,20 @@ struct render_basis
     v3 P;
 };
 
+struct render_entity_basis
+{
+    render_basis *Basis;
+    v2 Offset;
+    real32 OffsetZ;
+    real32 EntityZC;
+};
+
 // render_group_entry is a "compact discriminated union"
 enum render_group_entry_type
 {
-    RenderGroupEntry_Clear,
-    RenderGroupEntry_Rectangle,
+    RenderGroupEntryType_render_entry_clear,
+    RenderGroupEntryType_render_entry_bitmap,
+    RenderGroupEntryType_render_entry_rectangle,
 };
 struct render_group_entry_header
 {
@@ -22,15 +31,18 @@ struct render_entry_clear
     real32 R, G, B, A;
 };
 
+struct render_entry_bitmap
+{
+    render_group_entry_header Header;
+    loaded_bitmap *Bitmap;
+    render_entity_basis EntityBasis;
+    real32 R, G, B, A;
+};
+
 struct render_entry_rectangle
 {
     render_group_entry_header Header;
-    render_basis *Basis;
-    loaded_bitmap *Bitmap;
-    v2 Offset;
-    real32 OffsetZ;
-    real32 EntityZC;
-
+    render_entity_basis EntityBasis;
     real32 R, G, B, A;
     v2 Dim;
 };
@@ -44,70 +56,6 @@ struct render_group
     uint32_t PushBufferSize;
     uint8_t *PushBufferBase;
 };
-
-inline void *
-PushRenderElement(render_group *Group, uint32_t Size)
-{
-    void *Result = 0;
-
-    if((Group->PushBufferSize + Size) < Group->MaxPushBufferSize)
-    {
-        Result = (Group->PushBufferBase + Group->PushBufferSize);
-        Group->PushBufferSize += Size;
-    }
-    else
-    {
-        InvalidCodePath;
-    }
-
-    return(Result);
-}
-
-inline void
-PushPiece(render_group *Group, loaded_bitmap *Bitmap,
-          v2 Offset, real32 OffsetZ, v2 Align, v2 Dim, v4 Color, real32 EntityZC)
-{
-    render_group_entry *Piece = (render_group_entry *)PushRenderElement(Group, sizeof(render_group_entry));
-    Piece->Basis = Group->DefaultBasis;
-    Piece->Bitmap = Bitmap;
-    Piece->Offset = Group->MetresToPixels*V2(Offset.X, -Offset.Y) - Align;
-    Piece->OffsetZ = OffsetZ;
-    Piece->EntityZC = EntityZC;
-    Piece->R = Color.R;
-    Piece->G = Color.G;
-    Piece->B = Color.B;
-    Piece->A = Color.A;
-    Piece->Dim = Dim;
-}
-
-inline void
-PushBitmap(render_group *Group, loaded_bitmap *Bitmap,
-           v2 Offset, real32 OffsetZ, v2 Align, real32 Alpha = 1.0f, real32 EntityZC = 1.0f)
-{
-    PushPiece(Group, Bitmap, Offset, OffsetZ, Align, V2(0, 0), V4(1.0f, 1.0f, 1.0f, Alpha), EntityZC);
-}
-
-inline void
-PushRect(render_group *Group, v2 Offset, real32 OffsetZ,
-         v2 Dim, v4 Color, real32 EntityZC = 1.0f)
-{
-    PushPiece(Group, 0, Offset, OffsetZ, V2(0, 0), Dim, Color, EntityZC);
-}
-
-inline void
-PushRectOutline(render_group *Group, v2 Offset, real32 OffsetZ,
-                v2 Dim, v4 Color, real32 EntityZC = 1.0f)
-{
-    real32 Thickness = 0.1f;
-
-    // Top and Bottom.
-    PushPiece(Group, 0, Offset - V2(0, 0.5f*Dim.Y), OffsetZ, V2(0, 0), V2(Dim.X, Thickness), Color, EntityZC);
-    PushPiece(Group, 0, Offset + V2(0, 0.5f*Dim.Y), OffsetZ, V2(0, 0), V2(Dim.X, Thickness), Color, EntityZC);
-
-    // Left and Right.
-    PushPiece(Group, 0, Offset - V2(0.5f*Dim.X, 0), OffsetZ, V2(0, 0), V2(Thickness, Dim.Y), Color, EntityZC);
-    PushPiece(Group, 0, Offset + V2(0.5f*Dim.X, 0), OffsetZ, V2(0, 0), V2(Thickness, Dim.Y), Color, EntityZC);
-}
 
 #define GAME_RENDER_GROUP_H
 #endif
