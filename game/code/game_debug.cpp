c9e6265d0a0faa2b9ed9b233270408644b3de56b
Joshalosh <josh.jessopsmith@gmail.com>
2023-12-30 17:11:28 +1100

    Fix profiler and add clearer buttons

--- game/code/game_debug.cpp
+++ game/code/game_debug.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #include "game_debug.h"
+#include "game_debug_ui.cpp"
 
 struct debug_parsed_name
 {
@@ -85,15 +86,6 @@ GetElementFromGUID(debug_state *DebugState, char *GUID)
     return(Result);
 }
 
-inline b32
-DebugIDsAreEqual(debug_id A, debug_id B)
-{
-    b32 Result = ((A.Value[0] == B.Value[0]) &&
-                  (A.Value[1] == B.Value[1]));
-
-    return(Result);
-}
-
 inline debug_id
 DebugIDFromLink(debug_tree *Tree, debug_variable_link *Link)
 {
@@ -153,173 +145,6 @@ AddTree(debug_state *DebugState, debug_variable_group *Group, v2 AtP)
     return(Tree);
 }
 
-inline b32
-IsHex(char Char)
-{
-    b32 Result = (((Char >= '0') && (Char <= '9')) ||
-                  ((Char >= 'A') && (Char <= 'F')));
-
-    return(Result);
-}
-
-inline u32
-GetHex(char Char)
-{
-    u32 Result = 0;
-
-    if((Char >= '0') && (Char <= '9'))
-    {
-        Result = Char - '0';
-    }
-    else if((Char >= 'A') && (Char <= 'F'))
-    {
-        Result = 0xA + (Char - 'A');
-    }
-
-    return(Result);
-}
-
-internal rectangle2
-DEBUGTextOp(debug_state *DebugState, debug_text_op Op, v2 P, char *String, v4 Color = V4(1, 1, 1, 1))
-{
-    rectangle2 Result = InvertedInfinityRectangle2();
-    if(DebugState && DebugState->DebugFont)
-    {
-        render_group *RenderGroup = &DebugState->RenderGroup;
-        loaded_font *Font = DebugState->DebugFont;
-        ga_font *Info = DebugState->DebugFontInfo;
-
-        u32 PrevCodepoint = 0;
-        r32 CharScale = DebugState->FontScale;
-        r32 AtY = P.y;
-        r32 AtX = P.x;
-        for(char *At = String; *At;)
-        {
-            if((At[0] == '\\') &&
-               (At[1] == '#') &&
-               (At[2] != 0) &&
-               (At[3] != 0) &&
-               (At[4] != 0))
-            {
-                r32 CScale = 1.0f / 9.0f;
-                Color = V4(Clamp01(CScale*(r32)(At[2] - '0')),
-                           Clamp01(CScale*(r32)(At[3] - '0')),
-                           Clamp01(CScale*(r32)(At[4] - '0')),
-                           1.0f);
-                At += 5;
-            }
-            else if((At[0] == '\\') &&
-                    (At[1] == '^') &&
-                    (At[2] != 0))
-            {
-                r32 CScale = 1.0f / 9.0f;
-                CharScale = DebugState->FontScale*Clamp01(CScale*(r32)(At[2] - '0'));
-                At += 3;
-            }
-            else
-            {
-                u32 Codepoint = *At;
-                if((At[0] == '\\') &&
-                   (IsHex(At[1])) &&
-                   (IsHex(At[2])) &&
-                   (IsHex(At[3])) &&
-                   (IsHex(At[4])))
-                {
-                    Codepoint = ((GetHex(At[1]) << 12) |
-                                 (GetHex(At[2]) << 8) |
-                                 (GetHex(At[3]) << 4) |
-                                 (GetHex(At[4]) << 0));
-                    At += 4;
-                }
-
-                r32 AdvanceX = CharScale*GetHorizontalAdvanceForPair(Info, Font, PrevCodepoint, Codepoint);
-                AtX += AdvanceX;
-
-                if(Codepoint != ' ')
-                {
-                    bitmap_id BitmapID = GetBitmapForGlyph(RenderGroup->Assets, Info, Font, Codepoint);
-                    ga_bitmap *Info = GetBitmapInfo(RenderGroup->Assets, BitmapID);
-
-                    r32 BitmapScale = CharScale*(r32)Info->Dim[1];
-                    v3 BitmapOffset = V3(AtX, AtY, 0);
-                    if(Op == DEBUGTextOp_DrawText)
-                    {
-                        PushBitmap(RenderGroup, DebugState->TextTransform, BitmapID, BitmapScale,
-                                   BitmapOffset, Color, 1.0f);
-                        PushBitmap(RenderGroup, DebugState->ShadowTransform, BitmapID, BitmapScale,
-                                   BitmapOffset + V3(2.0f, -2.0f, 0.0f), V4(0, 0, 0, 1.0f), 1.0f);
-                    }
-                    else 
-                    {
-                        Assert(Op == DEBUGTextOp_SizeText);
-
-                        loaded_bitmap *Bitmap = GetBitmap(RenderGroup->Assets, BitmapID, RenderGroup->GenerationID);
-                        if(Bitmap)
-                        {
-                            used_bitmap_dim Dim = GetBitmapDim(RenderGroup, DefaultFlatTransform(), Bitmap, BitmapScale, BitmapOffset, 1.0f);
-                            rectangle2 GlyphDim = RectMinDim(Dim.P.xy, Dim.Size);
-                            Result = Union(Result, GlyphDim);
-                        }
-                    }
-                }
-
-                PrevCodepoint = Codepoint;
-
-                ++At;
-            }
-        }
-    }
-
-    return(Result);
-}
-
-internal void
-DEBUGTextOutAt(v2 P, char *String, v4 Color = V4(1, 1, 1, 1))
-{
-    debug_state *DebugState = DEBUGGetState();
-    if(DebugState)
-    {
-        render_group *RenderGroup = &DebugState->RenderGroup;
-        DEBUGTextOp(DebugState, DEBUGTextOp_DrawText, P, String, Color);
-    }
-}
-
-internal rectangle2
-DEBUGGetTextSize(debug_state *DebugState, char *String)
-{
-    rectangle2 Result = DEBUGTextOp(DebugState, DEBUGTextOp_SizeText, V2(0, 0), String);
-
-    return(Result);
-}
-
-inline r32
-GetLineAdvance(debug_state *DebugState)
-{
-    r32 Result = GetLineAdvanceFor(DebugState->DebugFontInfo)*DebugState->FontScale;
-    return(Result);
-}
-
-internal void
-DEBUGTextLine(char *String)
-{
-    debug_state *DebugState = DEBUGGetState();
-    if(DebugState)
-    {
-        render_group *RenderGroup = &DebugState->RenderGroup;
-
-        DEBUGTextOutAt(V2(DebugState->LeftEdge,
-                          DebugState->AtY - DebugState->FontScale*GetStartingBaselineY(DebugState->DebugFontInfo)), String);
-        DebugState->AtY -= GetLineAdvance(DebugState);
-    }
-}
-
-struct debug_statistic
-{
-    r64 Min;
-    r64 Max;
-    r64 Avg;
-    u32 Count;
-};
 inline void
 BeginDebugStatistic(debug_statistic *Stat)
 {
@@ -507,208 +332,6 @@ struct debug_variable_iterator
     debug_variable_link *Sentinel;
 };
 
-inline b32
-InteractionsAreEqual(debug_interaction A, debug_interaction B)
-{
-    b32 Result = (DebugIDsAreEqual(A.ID, B.ID) &&
-                  (A.Type == B.Type) &&
-                  (A.Target == B.Target) &&
-                  (A.Generic == B.Generic));
-
-    return(Result);
-}
-
-inline b32
-InteractionIsHot(debug_state *DebugState, debug_interaction B)
-{
-    b32 Result = InteractionsAreEqual(DebugState->HotInteraction, B);
-
-    if(B.Type == DebugInteraction_None)
-    {
-        Result = false;
-    }
-
-    return(Result);
-}
-
-inline debug_interaction
-SetPointerInteraction(debug_id DebugID, void **Target, void *Value)
-{
-    debug_interaction Result = {};
-    Result.ID = DebugID;
-    Result.Type = DebugInteraction_SetPointer;
-    Result.Target = Target;
-    Result.Pointer = Value;
-
-    return(Result);
-}
-
-inline debug_interaction
-SetUInt32Interaction(debug_id DebugID, u32 *Target, u32 Value)
-{
-    debug_interaction Result = {};
-    Result.ID = DebugID;
-    Result.Type = DebugInteraction_SetUInt32;
-    Result.Target = Target;
-    Result.UInt32 = Value;
-
-    return(Result);
-}
-
-inline layout_element
-BeginElementRectangle(layout *Layout, v2 *Dim)
-{
-    layout_element Element = {};
-
-    Element.Layout = Layout;
-    Element.Dim = Dim;
-
-    return(Element);
-}
-
-inline void
-MakeElementSizable(layout_element *Element)
-{
-    Element->Size = Element->Dim;
-}
-
-inline void
-DefaultInteraction(layout_element *Element, debug_interaction Interaction)
-{
-    Element->Interaction = Interaction;
-}
-
-inline void
-AdvanceElement(layout *Layout, rectangle2 ElRect)
-{
-    Layout->NextYDelta = Minimum(Layout->NextYDelta, GetMinCorner(ElRect).y - Layout->At.y);
-
-    if(Layout->NoLineFeed)
-    {
-        Layout->At.x = GetMaxCorner(ElRect).x + Layout->SpacingX;
-    }
-    else 
-    {
-        Layout->At.y += Layout->NextYDelta - Layout->SpacingY;
-        Layout->At.x = Layout->BaseCorner.x + Layout->Depth*2.0f*Layout->LineAdvance;
-
-        Layout->NextYDelta = 0.0f;
-    }
-}
-
-inline void
-EndElement(layout_element *Element)
-{
-    layout *Layout = Element->Layout;
-    debug_state *DebugState = Layout->DebugState;
-    object_transform NoTransform = DebugState->BackingTransform;
-
-    r32 SizeHandlePixels = 4.0f;
-
-    v2 Frame = {0, 0};
-    if(Element->Size)
-    {
-        Frame.x = SizeHandlePixels;
-        Frame.y = SizeHandlePixels;
-    }
-
-    v2 TotalDim = *Element->Dim + 2.0f*Frame;
-
-    v2 TotalMinCorner = V2(Layout->At.x,
-                           Layout->At.y - TotalDim.y);
-    v2 TotalMaxCorner = TotalMinCorner + TotalDim;
-
-    v2 InteriorMinCorner = TotalMinCorner + Frame;
-    v2 InteriorMaxCorner = InteriorMinCorner + *Element->Dim;
-
-    rectangle2 TotalBounds = RectMinMax(TotalMinCorner, TotalMaxCorner);
-    Element->Bounds = RectMinMax(InteriorMinCorner, InteriorMaxCorner);
-
-    if(Element->Interaction.Type && IsInRectangle(Element->Bounds, Layout->MouseP))
-    {
-        DebugState->NextHotInteraction = Element->Interaction;
-    }
-
-    if(Element->Size)
-    {
-        PushRect(&DebugState->RenderGroup, NoTransform, RectMinMax(V2(TotalMinCorner.x, InteriorMinCorner.y),
-                                                     V2(InteriorMinCorner.x, InteriorMaxCorner.y)), 0.0f,
-                 V4(0, 0, 0, 1));
-        PushRect(&DebugState->RenderGroup, NoTransform, RectMinMax(V2(InteriorMaxCorner.x, InteriorMinCorner.y),
-                                                     V2(TotalMaxCorner.x, InteriorMaxCorner.y)), 0.0f,
-                 V4(0, 0, 0, 1));
-        PushRect(&DebugState->RenderGroup, NoTransform, RectMinMax(V2(InteriorMinCorner.x, TotalMinCorner.y),
-                                                     V2(InteriorMaxCorner.x, InteriorMinCorner.y)), 0.0f,
-                 V4(0, 0, 0, 1));
-        PushRect(&DebugState->RenderGroup, NoTransform, RectMinMax(V2(InteriorMinCorner.x, InteriorMaxCorner.y),
-                                                     V2(InteriorMaxCorner.x, TotalMaxCorner.y)), 0.0f,
-                 V4(0, 0, 0, 1));
-
-        debug_interaction SizeInteraction = {};
-        SizeInteraction.Type = DebugInteraction_Resize;
-        SizeInteraction.P = Element->Size;
-
-        rectangle2 SizeBox = RectMinMax(V2(InteriorMaxCorner.x, TotalMinCorner.y),
-                                        V2(TotalMaxCorner.x, InteriorMinCorner.y));
-        PushRect(&DebugState->RenderGroup, NoTransform, SizeBox, 0.0f,
-                 (InteractionIsHot(DebugState, SizeInteraction) ? V4(1, 1, 0, 1) : V4(1, 1, 1, 1)));
-        if(IsInRectangle(SizeBox, Layout->MouseP))
-        {
-            DebugState->NextHotInteraction = SizeInteraction;
-        }
-    }
-
-    AdvanceElement(Layout, TotalBounds);
-}
-
-internal void
-BasicTextElement(layout *Layout, char *Text, debug_interaction ItemInteraction, 
-                 v4 ItemColor = V4(0.8f, 0.8f, 0.8f, 1), v4 HotColor = V4(1, 1, 1, 1))
-{
-    debug_state *DebugState = Layout->DebugState;
-
-    rectangle2 TextBounds = DEBUGGetTextSize(DebugState, Text);
-    v2 Dim = {GetDim(TextBounds).x, Layout->LineAdvance};
-
-    layout_element Element = BeginElementRectangle(Layout, &Dim);
-    DefaultInteraction(&Element, ItemInteraction);
-    EndElement(&Element);
-
-    b32 IsHot = InteractionIsHot(Layout->DebugState, ItemInteraction);
-
-    DEBUGTextOutAt(V2(GetMinCorner(Element.Bounds).x,
-                      GetMaxCorner(Element.Bounds).y - 
-                      DebugState->FontScale*GetStartingBaselineY(DebugState->DebugFontInfo)),
-                   Text, IsHot ? HotColor : ItemColor);
-}
-
-internal void
-BeginRow(layout *Layout)
-{
-    ++Layout->NoLineFeed;
-}
-
-internal void
-ActionButton(layout *Layout, char *Name, debug_interaction Interaction)
-{
-    BasicTextElement(Layout, Name, Interaction);
-}
-
-internal void
-BooleanButton(layout *Layout, char *Name, b32 Highlight, debug_interaction Interaction)
-{
-    BasicTextElement(Layout, Name, Interaction, Highlight ? V4(1, 1, 1, 1) : V4(0.5f, 0.5f, 0.5f, 1.0f));
-}
-
-internal void
-EndRow(layout *Layout)
-{
-    Assert(Layout->NoLineFeed > 0);
-    --Layout->NoLineFeed;
-
-    AdvanceElement(Layout, RectMinMax(Layout->At, Layout->At));
-}
-
 internal debug_view *
 GetOrCreateDebugViewFor(debug_state *DebugState, debug_id ID)
 {
@@ -854,6 +477,17 @@ DEBUG_REQUESTED(debug_id ID)
     return(Result);
 }
 
+internal u64
+GetTotalClocks(debug_element_frame *Frame)
+{
+    u64 Result = 0;
+    for(debug_stored_event *Event = Frame->OldestEvent; Event; Event = Event->Next)
+    {
+        Result += Event->ProfileNode.Duration;
+    }
+    return(Result);
+}
+
 global_variable v3 DebugColorTable[] =
 {
     {1, 0, 0},
@@ -869,17 +503,6 @@ global_variable v3 DebugColorTable[] =
     {0.5f, 0, 1},
     //{0, 0.5f, 1},
 };
-
-internal u64
-GetTotalClocks(debug_element_frame *Frame)
-{
-    u64 Result = 0;
-    for(debug_stored_event *Event = Frame->OldestEvent; Event; Event = Event->Next)
-    {
-        Result += Event->ProfileNode.Duration;
-    }
-    return(Result);
-}
 
 internal void
 DrawProfileBars(debug_state *DebugState, debug_id GraphID, rectangle2 ProfileRect, v2 MouseP,
@@ -921,7 +544,7 @@ DrawProfileBars(debug_state *DebugState, debug_id GraphID, rectangle2 ProfileRec
             _snprintf_s(TextBuffer, sizeof(TextBuffer),
                         "%s: %10ucy",
                         Element->GUID, (u32)Node->Duration);
-            DEBUGTextOutAt(MouseP + V2(0.0f, DebugState->MouseTextStackY), TextBuffer);
+            TextOutAt(DebugState, MouseP + V2(0.0f, DebugState->MouseTextStackY), TextBuffer);
             DebugState->MouseTextStackY -= GetLineAdvance(DebugState);
 
             // TODO: It would be better to generate a graph+element debug ID here
@@ -1021,7 +644,7 @@ DrawFrameBars(debug_state *DebugState, debug_id GraphID, rectangle2 ProfileRect,
                         _snprintf_s(TextBuffer, sizeof(TextBuffer),
                                     "%s: %10ucy",
                                     Element->GUID, (u32)Node->Duration);
-                        DEBUGTextOutAt(MouseP + V2(0.0f, DebugState->MouseTextStackY), TextBuffer);
+                        TextOutAt(DebugState, MouseP + V2(0.0f, DebugState->MouseTextStackY), TextBuffer);
                         DebugState->MouseTextStackY -= GetLineAdvance(DebugState);
 
                         debug_view *View = GetOrCreateDebugViewFor(DebugState, GraphID);
@@ -1090,7 +713,7 @@ DrawFrameSlider(debug_state *DebugState, debug_id SliderID, rectangle2 TotalRect
             {
                 char TextBuffer[256];
                 _snprintf_s(TextBuffer, sizeof(TextBuffer), "%u", FrameIndex);
-                DEBUGTextOutAt(MouseP + V2(0.0f, 10.0f), TextBuffer);
+                TextOutAt(DebugState, MouseP + V2(0.0f, 10.0f), TextBuffer);
 
                 DebugState->NextHotInteraction = 
                     SetUInt32Interaction(SliderID, &DebugState->ViewingFrameOrdinal, FrameIndex);
@@ -1167,7 +790,7 @@ DEBUGDrawElement(layout *Layout, debug_tree *Tree, debug_element *Element, debug
             layout_element LayEl = BeginElementRectangle(Layout, &Graph->Block.Dim);
             if((Graph->Block.Dim.x == 0) && (Graph->Block.Dim.y == 0))
             {
-                Graph->Block.Dim.x = 1800;
+                Graph->Block.Dim.x = 1400;
                 Graph->Block.Dim.y = 480;
             }
 
@@ -1200,7 +823,7 @@ DEBUGDrawElement(layout *Layout, debug_tree *Tree, debug_element *Element, debug
 
         case DebugType_FrameSlider:
         {
-            v2 Dim = {1800, 32};
+            v2 Dim = {1400, 32};
             layout_element LayEl = BeginElementRectangle(Layout, &Dim);
             EndElement(&LayEl);
 
@@ -1272,13 +895,7 @@ DEBUGDrawMainMenu(debug_state *DebugState, render_group *RenderGroup, v2 MouseP)
         Tree != &DebugState->TreeSentinel;
         Tree = Tree->Next)
     {
-        layout Layout = {};
-        Layout.DebugState = DebugState;
-        Layout.MouseP = MouseP;
-        Layout.BaseCorner = Layout.At = Tree->UIP;
-        Layout.LineAdvance = DebugState->FontScale*GetLineAdvanceFor(DebugState->DebugFontInfo);
-        Layout.SpacingY = 4.0f;
-        Layout.SpacingX = 4.0f;
+        layout Layout = BeginLayout(DebugState, MouseP, Tree->UIP);
 
         u32 Depth = 0;
         debug_variable_iterator Stack[DEBUG_MAX_VARIABLE_STACK_DEPTH];
@@ -1315,7 +932,7 @@ DEBUGDrawMainMenu(debug_state *DebugState, render_group *RenderGroup, v2 MouseP)
 
                         char *Text = Link->Children->Name;
 
-                        rectangle2 TextBounds = DEBUGGetTextSize(DebugState, Text);
+                        rectangle2 TextBounds = GetTextSize(DebugState, Text);
                         v2 Dim = {GetDim(TextBounds).x, Layout.LineAdvance};
 
                         layout_element Element = BeginElementRectangle(&Layout, &Dim);
@@ -1325,9 +942,10 @@ DEBUGDrawMainMenu(debug_state *DebugState, render_group *RenderGroup, v2 MouseP)
                         b32 IsHot = InteractionIsHot(DebugState, ItemInteraction);
                         v4 ItemColour = IsHot ? V4(1, 1, 0, 1) : V4(1, 1, 1, 1);
 
-                        DEBUGTextOutAt(V2(GetMinCorner(Element.Bounds).x,
-                                          GetMaxCorner(Element.Bounds).y - DebugState->FontScale*GetStartingBaselineY(DebugState->DebugFontInfo)),
-                                       Text, ItemColour);
+                        TextOutAt(DebugState, V2(GetMinCorner(Element.Bounds).x,
+                                                 GetMaxCorner(Element.Bounds).y - DebugState->FontScale *
+                                                 GetStartingBaselineY(DebugState->DebugFontInfo)),
+                                  Text, ItemColour);
 
                         if(View->Collapsible.ExpandedAlways)
                         {
@@ -1363,6 +981,8 @@ DEBUGDrawMainMenu(debug_state *DebugState, render_group *RenderGroup, v2 MouseP)
                 DebugState->NextHotInteraction = MoveInteraction;
             }
         }
+
+        EndLayout(&Layout);
     }
 
 #if 0
